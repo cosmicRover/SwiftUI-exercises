9e23a0ae75044665b53cffe405817de12d985fd8
joypaul <jpaul3250@gmail.com>
2020-02-03 23:29:34 -0500

    chapter 2 finished

--- BuildingListsAndNavigation/StartingPoint/Landmarks/Landmarks/LandmarkList.swift
+++ BuildingListsAndNavigation/StartingPoint/Landmarks/Landmarks/LandmarkList.swift
@@ -11,15 +11,24 @@ import SwiftUI
 struct LandmarkList: View {
     var body: some View {
         /*a dynamic list of items*/
-        /*MARK:  TODO: need to get id for ListView working so that dynamic list building works*/
-        List(landmarkData, id: \.id){
-            
-        }
+        /*pass id param to List() to get hold of unique ids of each row. Since LandmarkData already
+         has an id, we can call landmarkdata directly*/
+        NavigationView{//embeded in a nav bar, then -> provide a navigation link
+            List(landmarkData){landmark in
+                NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
+                    LandmarkRow(landmark: landmark)
+                }
+            }
+        }.navigationBarTitle(Text("Landmarks"))//nav bar takes in a titile (not appearning for some reason)
     }
 }
 
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
-        LandmarkList()
+        //to preview on varoius devices use preview device widgets. can also be used as list of devices
+        ForEach(["iPhone SE", "iPhone X"], id: \.self){ device in
+            LandmarkList().previewDevice(PreviewDevice(rawValue: device))
+            .previewDisplayName(device)
+        }
     }
 }
